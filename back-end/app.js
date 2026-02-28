c8890d6d72c40c1ed0f6f8766f46d74e9cbe8647
Kasarla Mahesh <kasarlamahesh4@gmail.com>
2026-02-28 12:11:49 +0530

    added new port

--- back-end/app.js
+++ back-end/app.js
@@ -40,6 +40,7 @@ app.use(express.urlencoded({ extended: true }));
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:8080",
+  "http://localhost:8081",
   "http://localhost:3002",
   "http://localhost:3001",
   "https://upanaya.vercel.app",
